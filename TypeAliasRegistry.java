2c9244097eb372ed06516e4220fcd4760fe51424
lanyage <38199921+lanyage@users.noreply.github.com>
2018-05-06 17:11:30 +0800

    Update TypeAliasRegistry.java

--- TypeAliasRegistry.java
+++ TypeAliasRegistry.java
@@ -6,12 +6,16 @@ import java.util.Map;
 /**
  * Created by lanyage on 2018/5/6.
  */
+/** 用于注册类和类的别名*/
 public class NameRegistry {
+    /** 实际上注册是将想要注册的东西存放到一个hashMap中去 */
     private final Map<String, Class<?>> TYPE_ALIASES = new HashMap<>();
-
+    /** 构造器,默认注册一些类和类的别名,mybatis框架就会在程序运行的时候注册一些类如String.class */
     public NameRegistry() {
         this.register("Student", Student.class);
     }
+
+    /** 注册动作*/
     public void register(String alias, Class<?> clazz) {
         TYPE_ALIASES.put(alias, clazz);
     }
