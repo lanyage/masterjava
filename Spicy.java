6081396b0151eeb118994c3441306c0eca5c8ca8
lanyage <38199921+lanyage@users.noreply.github.com>
2018-05-10 11:03:56 +0800

    添加主是
    枚举类的正确使用方法

--- Spicy.java
+++ Spicy.java
@@ -3,6 +3,11 @@ package Register;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Created by lanyage on 2018/5/10.
+ *
+ * 这个枚举类使用了常用的表现手法,默认自持有所有常量的字典,可以非常方便的根据枚举类型的名字获得枚举类型的常量对象
+ */
 public enum Spicy {
     HOT("hot"),MIDDLE("middle"),NORMAL("normal");
     private final String detail;
